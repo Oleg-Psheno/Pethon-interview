c60c3a2a60851e9c52b1f335176dbab85ec42796
Oleg <bezrukov-oa@nm.ru>
2021-04-22 16:43:07 +0300

    Add pop-up for creating new item

--- lesson4/mainapp/views.py
+++ lesson4/mainapp/views.py
@@ -6,7 +6,15 @@ from django.core import serializers
 
 def index(request):
     goods = Good.objects.all()
-    context = {'goods': goods}
+    if request.method == 'POST':
+        form = AddForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = AddForm()
+    context = {'goods': goods,
+               'form': form}
     return render(request, 'index.html', context=context)
 
 
